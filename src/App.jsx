5932642da59a59c23570cd371a191881248f4b24
henriquelago2017 <henriquelago2017@gmail.com>
2025-05-08 20:01:34 +0000

    primeiro card e componente de input button

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,17 @@
-import istar from "./assets/icon-star.svg"
 import { useState } from "react";
 import ilustration from "./assets/illustration-thank-you.svg"
+import { CardInical } from "./Components/CardInicial";
 
 export function App(){
   const[nota, setNota] = useState(0);
   const[submited, setSubimeted] = useState(false)
 
 
-  function handleTrocarNota(nota){
-    setNota(nota)
-
-    console.log(nota)
-  }
-
-  function handleSubmit(){
-    if(nota !== 0){
-      setSubimeted(true)
-      return
-    }
-    alert("Please, select a rate button")
-  }
+ 
 
   return(
     submited === false ? (
-        <div className="max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass">
-        
-        <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
-          <img src={istar} alt="" />
-        </div>
-
-        <h1 className="text-2xl font-bold mb-2.5 " >How did we do?</h1>
-        <p className="text-sm text-light-grey mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-
-        <div className="flex justify-between mb-6" >
-          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white sm:w-13 sm:h13 cursor-pointer" onClick={() => handleTrocarNota(1)}/>
-          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white sm:w-13 sm:h13 cursor-pointer" onClick={() => handleTrocarNota(2)}/>
-          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white sm:w-13 sm:h13 cursor-pointer" onClick={() => handleTrocarNota(3)}/>
-          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white sm:w-13 sm:h13 cursor-pointer" onClick={() => handleTrocarNota(4)}/>
-          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white sm:w-13 sm:h13 cursor-pointer" onClick={() => handleTrocarNota(5)}/>
-        </div>
-
-        <button className="bg-orange w-full py-3 rounded-3xl uppercase text-sm tracking-1 font-bold hover:bg-white hover:text-orange cursor-pointer" onClick={handleSubmit}>Submit</button>
-      </div>
+        <CardInical nota={nota} setNota= {setNota} setSubimeted={setSubimeted} />
     ) : (
       <div className="max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass text-center">
         <img className="mx-auto mb-6" src={ilustration} alt="" />
---
+++ src/Components/CardInicial.jsx
@@ -0,0 +1,42 @@
+import istar from "../assets/icon-star.svg"
+import { InputButton } from "./InputButton"
+
+export function CardInical({nota ,setNota, setSubimeted}){
+    function handleSubmit(){
+        if(nota !== 0){
+          setSubimeted(true)
+          return
+        }
+        alert("Please, select a rate button")
+      }
+    
+
+    function handleTrocarNota(nota){
+        setNota(nota)
+    
+        console.log(nota)
+      }
+
+    return(
+        
+        <div className="max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass">
+        
+        <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
+          <img src={istar} alt="" />
+        </div>
+
+        <h1 className="text-2xl font-bold mb-2.5 " >How did we do?</h1>
+        <p className="text-sm text-light-grey mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+
+        <div className="flex justify-between mb-6" >
+          <InputButton  valor={1} handleTrocarNota={handleTrocarNota}/>
+          <InputButton  valor={2} handleTrocarNota={handleTrocarNota}/>
+          <InputButton  valor={3} handleTrocarNota={handleTrocarNota}/>
+          <InputButton  valor={4} handleTrocarNota={handleTrocarNota}/>
+          <InputButton  valor={5} handleTrocarNota={handleTrocarNota}/>
+        </div>
+
+        <button className="bg-orange w-full py-3 rounded-3xl uppercase text-sm tracking-1 font-bold hover:bg-white hover:text-orange cursor-pointer" onClick={handleSubmit}>Submit</button>
+      </div>
+    )
+}
